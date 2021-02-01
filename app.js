7f8b339f56d551fccef211761b279ebf5a8e914e
noirkotyara <noirkotyara@gmail.com>
2021-02-01 09:18:39 +0200

    heroku mern deploy

--- app.js
+++ app.js
@@ -2,18 +2,21 @@ const express = require ('express')
 const config = require('config')
 const mongoose = require('mongoose')
 const path = require('path')
+const cors = require('cors')
 
 const app = express()
 const PORT = process.env.PORT || config.get('port')  
 
-app.use(express.static(path.join(__dirname)));
 //middleware
 app.use(express.json({extended: true}))
 app.use('/api/auth', require('./routes/auth-routes'))
 app.use('/api/link', require('./routes/link-routes'))
 app.use('/t', require('./routes/redirect-routes'))
+app.use(cors())
+
 
 const start = async() => {
+    console.log('START')
     try {
         await mongoose.connect(process.env.MONGODB_URI || config.get('mongoUri'), {
             useNewUrlParser: true,
@@ -34,12 +37,14 @@ const start = async() => {
 //fix: refactor hard coded connection string to Heroku config var
 
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static('client/build'))
+  app.use(express.static(path.join(__dirname, 'client/build')))
   
   app.get('*', (req, res) => {
-          res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
+          res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
         });
 }
+
+
 start()
 
 
